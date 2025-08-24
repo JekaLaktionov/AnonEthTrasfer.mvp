2ef26a48678207592b485c1edb83e55ce1e8ffb8
Yevhenii Laktionov <90025117+JekaLaktionov@users.noreply.github.com>
2025-08-24 16:51:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ document.addEventListener("DOMContentLoaded", () => {
 import { ethers } from "https://cdn.jsdelivr.net/npm/ethers@6.8.1/dist/ethers.min.js";
 import { contractABI } from './abi.js';
 
-const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+const contractAddress = "-";
 
 let provider;
 let signer;
@@ -255,6 +255,7 @@ async function withdraw(e) {
 
     }
 }
+
 
 
 
